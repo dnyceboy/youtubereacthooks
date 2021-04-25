98866d78af6ae0cb6c58f73a7bbc17dd4e5d3369
Damien <dnyceboy@gmail.com>
2021-04-25 16:17:38 -0400

    Updates

---
+++ precache-manifest.3a020ee71dfb5736ec0e9e2a5f21372a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd9fd80366cc20ab0ba2d6e81d0713bb",
+    "url": "/youtubereacthooks/index.html"
+  },
+  {
+    "revision": "6bdf12ba0d28d7ed0649",
+    "url": "/youtubereacthooks/static/css/main.29be0024.chunk.css"
+  },
+  {
+    "revision": "01e8959a919c61ddcda1",
+    "url": "/youtubereacthooks/static/js/2.3dc8c74d.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/youtubereacthooks/static/js/2.3dc8c74d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6bdf12ba0d28d7ed0649",
+    "url": "/youtubereacthooks/static/js/main.bb0d4f0f.chunk.js"
+  },
+  {
+    "revision": "1cc185ffdbd076423a9d",
+    "url": "/youtubereacthooks/static/js/runtime-main.7087d2f3.js"
+  }
+]);
